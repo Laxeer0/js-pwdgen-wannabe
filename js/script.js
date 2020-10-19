64fbc5e506926b9e4049af53776e4da5e5e873e2
Giuseppe <laxeer0@gmail.com>
2020-10-19 13:01:04 +0200

    Project Complete

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var nome = prompt('Come ti chiami?')
+var cognome = prompt('Come fai di cognome?')
+var colorepreferito = prompt('Qual è il tuo colore preferito?')
+
+var output = nome + cognome + colorepreferito + "19";
+
+document.getElementById("output").innerHTML = output;
